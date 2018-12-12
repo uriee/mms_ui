43a509fb46b6acfcb5cbb49359dd930c3d037af5
Uri Evenezra <uri.e@cbt.co.il>
2018-12-12 08:20:17 +0000

    Added schemas for Maintenance Plan

---
+++ src/pages/schemas/Mnt_plan_items.js
@@ -0,0 +1,70 @@
+/*
+The label in forms is determents by <FormattedMessage id=forms+entity+field /> forms+entity+field
+*/
+const mnt_plan_items = { 
+  "entity": "mnt_plan_items",
+  "title" :"Maintenance Plan's Resources",
+  'defaultKey' : 'name',  
+  "forms": {
+    "insert": {
+      "steps": [
+        {
+          "title": "Resource",
+          "fields": [
+            {
+              "field": "resource_name",
+              "placeholder": "Resource Name",
+              "style": {
+                "width": "80%",
+              }
+            }
+          ],
+          "format": [
+            [0]
+          ]
+        }
+      ]
+    },
+    "update": {
+      "steps": [
+        {
+          "title": "Resource",
+          "fields": [
+            {
+              "field": "resource_name",
+              "placeholder": "Resource Name",
+              "style": {
+                "width": "80%",
+              }
+            }
+          ],
+          "format": [
+            [0]
+          ]
+        }
+      ]
+    }
+  },
+  "fields": {
+    "id": {
+      "required": false
+    },
+    "name": {
+      "dataIndex":"name",
+      "sorter": true,
+    }, 
+    "resource_name": {
+      "dataIndex":"resource_name",
+      "inputMethod": "select",
+      "chooser": "resources",      
+      "sorter": true,
+      "inputRules": [
+        {
+          "required": true,
+          "message": "This field is required"
+        },
+      ]
+    },       
+  }
+}
+export {mnt_plan_items}
---
+++ src/pages/schemas/Mnt_plans.js
@@ -0,0 +1,229 @@
+/*
+The label in forms is determents by <FormattedMessage id=forms+entity+field /> forms+entity+field
+*/
+const mnt_plans = { 
+  "entity": "mnt_plan",
+  "title" :"Mnt_plans",
+  "forms": {
+    "insert": {
+      "steps": [
+        {
+          "title": "Details",
+          "fields": [
+            {
+              "field": "mnt_plan_name",
+              "placeholder": "Mnt_plan Name",
+              "style": {
+                "width": "80%",
+              }
+            },
+            {
+              "field": "start_date",
+              "placeholder": "Start Date",
+              "style": {
+                "width": "80%",
+              }
+            },
+            {
+              "field": "end_date",
+              "placeholder": "End Date",
+              "style": {
+                "width": "80%",
+              }
+            },            
+            {
+              "field": "repeat",
+              "placeholder": "Repeat Every ...",
+              "style": {
+                "width": "80%",
+              }
+            }, 
+            {
+              "field": "reschedule",
+              "placeholder": "Reschedule",
+              "style": {
+                "width": "80%",
+              }
+            }
+          ],
+          "format": [
+            [0],
+            [1,2],
+            [3,4]
+          ]
+        },
+        {
+          "title": "Descriptions",
+          "fields": [
+            {
+              "field": "description",
+              "placeholder": "Description in English",
+              "style": {
+                "width": "80%",
+              }
+            },
+            {
+              "field": "description_t",
+              "placeholder": "Description",
+              "style": {
+                "width": "80%",
+              }
+            }
+          ],
+          "format": [
+            [0],
+            [1],
+          ]
+        }        
+      ]
+    },
+    "update": {
+      "steps": [
+        {
+          "title": "Details",
+          "fields": [
+            {
+              "field": "mnt_plan_name",
+              "placeholder": "Mnt_plan Name",
+              "style": {
+                "width": "80%",
+              }
+            },
+            {
+              "field": "start_date",
+              "placeholder": "Start Date",
+              "style": {
+                "width": "80%",
+              }
+            },
+            {
+              "field": "end_date",
+              "placeholder": "End Date",
+              "style": {
+                "width": "80%",
+              }
+            },            
+            {
+              "field": "repeat",
+              "placeholder": "Repeat Every ...",
+              "style": {
+                "width": "50%",
+              }
+            },
+            {
+              "field": "reschedule",
+              "placeholder": "Reschedule",
+              "style": {
+                "width": "80%",
+              }
+            }
+          ],
+          "format": [
+            [0],
+            [1,2],
+            [3,4]
+          ]
+        },
+        {
+          "title": "Descriptions",
+          "fields": [
+            {
+              "field": "description",
+              "placeholder": "Description in English",
+              "style": {
+                "width": "80%",
+              }
+            },
+          ],
+          "format": [
+            [0]
+          ]
+        }        
+      ]
+    }
+  },
+  "fields": {
+    "id": {
+      "required": false
+    },
+    "mnt_plan_name": {
+      "dataIndex":"name",
+      "inputMethod": "input",
+      "link" : '/router/mnt_plan_items',      
+      "sorter": true,
+      "inputRules": [
+        {
+          "required": true,
+          "message": "This field is required"
+        },
+        {
+          "min": 2,
+          "message": " minimum 2 character"
+        }
+      ]
+    },    
+    "description": {
+      "dataIndex": "description",
+      "inputMethod": "textArea",
+      "sorter": true,
+      "inputRules": [
+        {
+          "required": true,
+          "message": "This field is required"
+        },
+        {
+          "min": 5,
+          "message": " minimum 5 character"
+        }
+      ]
+    },
+    "description_t": {
+      "inputMethod": "textArea",
+      "sorter": true,
+      "required": false,      
+      "inputRules": [
+        {
+          "required": true,
+          "message": "This field is required"
+        },
+        {
+          "min": 5,
+          "message": " minimum 5 character"
+        }
+      ]
+    },    
+    "start_date": {
+      "dataIndex":"start_date",
+      "inputMethod": "timestamp",
+      "sorter": true,
+      "inputRules": [
+        {
+          "required": true,
+          "message": "This field is required"
+        },
+      ]
+    },
+    "end_date": {
+      "dataIndex":"end_date",
+      "inputMethod": "timestamp",
+      "sorter": true,
+      "inputRules": [
+        {
+          "required": true,
+          "message": "This field is required"
+        },
+      ]
+    },
+    "repeat": {
+      "dataIndex":"repeat",
+      "inputMethod": "number",
+      "sorter": true
+    },
+    "reschedule": {
+      "dataIndex":"reschedule",
+      "inputMethod": "bool",
+      "sorter": true
+    },    
+  }
+}
+export {mnt_plans}
