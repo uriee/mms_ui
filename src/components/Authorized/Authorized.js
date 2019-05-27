e95e64b54b2dab05e181a3e15de2f316efacc312
uriee <uevenezra@gmail.com>
2019-05-27 13:04:48 +0300

    Fix: first login bug

--- src/components/Authorized/Authorized.js
+++ src/components/Authorized/Authorized.js
@@ -3,6 +3,7 @@ import CheckPermissions from './CheckPermissions';
 const Authorized = ({ children, authority, noMatch = null }) => {
   const childrenRender = typeof children === 'undefined' ? null : children;
   console.log("authorityauthorityauthorityauthorityauthority:",authority)// = authority || ['admin']
+  authority = authority || ['admin']  
   return CheckPermissions(authority, childrenRender, noMatch);
 };
 
